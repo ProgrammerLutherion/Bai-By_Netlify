9814c5498592b1da9cc077a8f225b179fd2c7e60
ProgrammerLutherion <XD9002@gmail.com>
2022-06-13 20:47:03 +0200

    Cambios lista

--- src/app/Pokemons/Pokemon.ts
+++ src/app/Pokemons/Pokemon.ts
@@ -1,13 +1,15 @@
 export interface IPokemon{
   id:number
-  name:{english:string}
-  type:string[]
-  base:{
-    HP:number
-    Attack: number
-    Defense: number
-    'Sp.Attack': number
-    'Sp. Defense': number
-    Speed:number
-  }
+  name_english:string
+  name_japanese:string
+  name_chinese:string
+  name_french:string
+  type_0:number
+  type_1:number
+  base_HP:number
+  base_Attack:number
+  base_Defense:number
+  base_SpAttack:number
+  base_SpDefense:number
+  base_Speed:number
 }
